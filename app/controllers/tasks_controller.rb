0beeddd37295f6d17c18a3b335405a3c00d5a55a
koji.harada <bb.sbr7.dd@gmail.com>
2020-10-21 05:47:40 +0000

    tasklist Ver3.0.1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   
   def index
-    if logged_in?
-     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
